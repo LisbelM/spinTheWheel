045e542223a222f3c1a6bc73f95d60b82828ee6f
LisbelM <lisbelbmartinez@gmail.com>
2022-05-12 18:10:43 -0600

    Start of submission List - RealTime Firestore DB implemented. Missing - designs & self add functionality

--- pages/submissionList.js
+++ pages/submissionList.js
@@ -1,34 +1,52 @@
-import styles from "../styles/submissonList.module.css"
+import styles from "../styles/submissionList.module.css"
 
 import { collection, addDoc, getDocs } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore'
 import firebase from "../firebase/clientApp";
 import "firebase/compat/firestore";
-const db = firebase.firestore();
+
 
 const submissionList = () => {
-
+    const db = firebase.firestore();
     const [subs, subsLoading, subsError] = useCollection(
-
         db.collection("wheelSubmissions"),
         {}
-
     );
 
-    <div clasName={ styles.submissionList }>
+    console.log(subs)
 
-        {subsLoading && <span>Submissions: Loading...</span>}
-        {subs && subs.docs.map((doc) => (
-            <div>
-                <div className={styles.subOdds}></div>
-                    {JSON.stringify(doc.data())},{' '}
-                <div className={styles.subChecked}></div>
-                <span className={styles.clearSubX}>X</span>
+    return (
+        <div className={styles.submissionList}>
+
+            <span>Wheel Items ()</span>
+
+            <div className={styles}>
+                <div className={styles.submission}>
+                    <input className={styles.subOdds} placeholder="1"></input>
+                    <input className={styles.submissionInfoInput} placeholder="Enter an item"></input>
+                </div>
             </div>
-        ))}
 
+            <hr></hr>
 
-    </div>
+            {subsError && <strong>Error: {JSON.stringify(subsError)}</strong>}
+            {subsLoading && <span>Submissions: Loading...</span>}
+            {subs && subs.docs.map((doc) => (
+                <div>
+                    <div className={styles.submission}>
+                        <input className={styles.subOdds} placeholder="1"></input>
+                        <div className={styles.submissionInfo}>
+                            <span className={styles.submissionDesc}>{JSON.stringify(doc.data().submission)}</span>
+                            <span className={styles.submissionUsr}>{JSON.stringify(doc.data().usrName)}</span>
+                        </div>
+                        <div className={styles.subChecked}></div>
+                    </div>
+                    <span className={styles.clearSubX}>X</span>
+                </div>
+            ))}
+
+        </div>
+    )
 
 }
 
