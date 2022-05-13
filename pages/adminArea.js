e2da6909b01996b7b0a67f0dcd8dd6bfe4d5d9be
LisbelM <lisbelbmartinez@gmail.com>
2022-05-13 08:56:36 -0600

    winner popup functionality complete

--- pages/adminArea.js
+++ pages/adminArea.js
@@ -1,14 +1,37 @@
 import styles from "../styles/adminArea.module.css"
+import popupStyles from "../styles/popup.module.css"
+
 import { SubmissionList } from "./submissionList"
 import WheelComponent from "./wheelComponent";
-import { createContext } from "react"
+import { createContext, useState } from "react"
 
 import { collection, addDoc, getDocs } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore'
 import firebase from "../firebase/clientApp";
 import "firebase/compat/firestore";
 
-export const submissionListContext = createContext({ subs : [] , subsLoading : [], subsError : []});
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+
+import Modal from 'react-modal';
+Modal.defaultStyles.overlay.backgroundColor = 'rgba(0,0,0,0.2)'; /*overlay*/
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        backgroundColor: 'var(--navyBlue)',
+        width: '25%',
+        border: 'none'
+    },
+};
+
+export const submissionListContext = createContext({ subs: [], subsLoading: [], subsError: [] });
+export const winnerContext = createContext();
+
 
 export default function App() {
 
@@ -19,22 +42,45 @@ export default function App() {
         {}
     }*/
 
+    const [popUpValues, setWinner] = useState({
+        popupBool: false,
+        winnerSubmission: '',
+        winnerUsrName: '',
+    })
+
     const [subs, subsLoading, subsError] = useCollection(
         db.collection("wheelSubmissions"),
         {}
     );
 
+    function closeModal() {
+        setWinner({ ...popUpValues, popupBool: false });
+    }
+
     return (
         <submissionListContext.Provider value={{ subs, subsLoading, subsError }}>
-            <div className={styles.adminArea}>
+        <winnerContext.Provider value={{ popUpValues, setWinner }}>
+                <div className={styles.adminArea}>
                 <WheelComponent />
                 <SubmissionList />
 
                 <div className={styles.adminBtnsContainer}>
                     <button className={styles.spinHistBtn}>Spin History</button>
                     <button className={styles.endWheelBtn}>End Wheel</button>
-                </div>
+                 </div>
+
+                <Modal
+                    isOpen={popUpValues.popupBool}
+                    style={customStyles}
+                >
+                    <h2 className={popupStyles.popupTitle}>{popUpValues.winnerSubmission}</h2>
+                    <span className={popupStyles.subTitle}>{popUpValues.winnerUsrName}</span>
+                    <button className={popupStyles.doneBtn} onClick={closeModal}>Done</button>
+                    <button className={popupStyles.hideChoiceBtn} onClick={closeModal}>Hide Choice</button>
+                </Modal>
+
             </div>
+        </winnerContext.Provider>
         </submissionListContext.Provider>
     );
 }
